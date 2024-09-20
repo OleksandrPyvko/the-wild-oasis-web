4eebb6efa42aa279bc44e856f9ea77d0f4a84326
Oleksandr Pyvko <pyvko.a.g@gmail.com>
2024-09-20 16:46:27 +0300

    delete reservation feature

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -71,7 +71,7 @@ function ReservationCard({ booking }) {
       </div>
 
       <div className="flex flex-col border-l border-primary-800 w-[100px]">
-        {isPast ? null : (
+        {!isPast(startDate) ? (
           <>
             <Link
               href={`/account/reservations/edit/${id}`}
@@ -82,7 +82,7 @@ function ReservationCard({ booking }) {
             </Link>
             <DeleteReservation bookingId={id} />
           </>
-        )}
+        ) : null}
       </div>
     </div>
   );
